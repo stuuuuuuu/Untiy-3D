72e76537ee89ccbeafc83b00bee3dfc9a67c56cc
st33333 <526809654@qq.com>
2018-01-30 11:50:46 +0800

    Untiy3D

---
+++ Roll/Assets/PlayerControlle.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PlayerControlle : MonoBehaviour {
+	public float speed;
+	public float count;
+	public Text countText;
+	// Use this for initialization
+	void Start () {
+		count = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		GetComponent<Rigidbody> ().AddForce (movement * speed * Time.deltaTime);
+	}
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Pick Up") {
+			other.gameObject.SetActive (false);
+			count++;
+			setCountText ();
+		}
+	}
+	void setCountText(){
+		countText.text = "Count:" + count;
+	}
+
+}
